dd849422968e2845944d2dc94784fdd7d36eb6ea
MMaicke <mtmaicke@fullsail.edu>
2015-05-07 13:35:58 -0400

    hopefully this works
    work

---
+++ class/js/script.js
@@ -0,0 +1,27 @@
+/**
+ * Created by matthewmaicke on 5/7/15.
+ * SDI Day TWO
+ */
+
+
+// This is a single line comment
+
+// these are variables
+var myCellNumber    =   "2246279175";
+var milesToClass    =   2.5;
+var construction    =   false;
+var myFavoriteStore =   "Gamestop"
+
+
+//confirm
+
+construction = confirm("is there construction? click ok for yes");
+
+// my output
+
+console.log ("i my number is " + myCellNumber); //first output
+console.log ("i buy games at " + myFavoriteStore); //second output
+console.log ("going to class i travel" + milesToClass + "miles"); //third output
+console.log ("it's " + construction + " that there is construction"); //fourth output
+
+
