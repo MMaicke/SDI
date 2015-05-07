58ecfe30ed7147ae524dd5d84a4c35ec18a38569
MMaicke <mtmaicke@fullsail.edu>
2015-05-07 14:47:14 -0400

    lab2 done
    done

--- Maicke_Matthew_lab2.js
+++ Maicke_Matthew_lab2.js
@@ -1,1 +1,33 @@
 alert("JavaScript works!");
+
+// "this is were my work starts"
+
+//part 1
+
+var myname = "Matthew Maicke";
+
+var favoriteNFLTeam ="\"DA Bears\"";
+
+var favoritenumber = 13;
+
+var theNFLBearsAreCursed = false;
+
+// confirm
+
+//part 3
+
+theNFLBearsAreCursed = confirm("The Chicago Bears are cursed click ok for true cancel for false");
+//output
+
+// part 2
+
+console.log ("My name is " + myname + " and my favorite number is " + favoritenumber);
+
+console.log (" and my favorite football team is" + favoriteNFLTeam +" despite they are cursed " + theNFLBearsAreCursed);
+
+
+
+favoritenumber = prompt("New favorite number?");
+
+console.log ("my name is " + myname + " my new favorite number is " +favoritenumber);
+
