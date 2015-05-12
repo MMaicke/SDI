b3cc338ec3d69ec47c2d7b290f93159edc09fd44
MMaicke <mtmaicke@fullsail.edu>
2015-05-12 14:11:30 -0400

    lab

---
+++ day4/lab4/logic/logic-tables.js
@@ -0,0 +1,36 @@
+//  NAME:  Enter your name here
+//  DATE:  Enter the current date
+// Scalable Data Infrastructures
+// Day 4 Lab
+//  Assignment 1
+//  Using logical operators
+
+
+/*
+var p;
+var q;
+var r;
+
+p = confirm("First value:  Click OK for true or Cancel for false.");
+q = confirm("Second value:  Click OK for true or Cancel for false.");
+r = confirm("third value:  click ok for true or cancel for false.")
+if (p ||(q||r)) {
+    console.log("With " + p + " and " + q + " and "+ r +", the outcome is TRUE.");
+} else {
+    console.log("With " + p + " and " + q + " and "+ r + ", the outcome is FALSE.");
+};
+*/
+
+
+
+// problem 1
+
+
+var youth = (<10);
+var senior =(>55);
+var age = ""
+
+age = prompt("type in age")
+
+
+console.log(age)
