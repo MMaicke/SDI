8d81d19cb06cb06ef2ec04767e7ce4d893a2eb5c
MMaicke <mtmaicke@fullsail.edu>
2015-05-14 13:30:18 -0400

    basic setup

---
+++ day5/lab5/lab5.js
@@ -0,0 +1,27 @@
+//alert("JavaScript works!");
+
+
+//var
+
+var firstnumber  = parseFloat(prompt("enter the first number."));
+var secondnumber = parseFloat(prompt("enter the second number."));
+var symbol       = prompt("please enter + - / or *");
+
+
+//functions
+
+function add (add) {
+
+}
+function subtract (minus){
+
+}
+function multiply (times){
+
+}
+function divide (divide){
+
+}
+
+
+console.log( firstnumber + ' ' + secondnumber )
