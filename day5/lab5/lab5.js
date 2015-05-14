a2259c878acd9e914fce63742c0d1a050e0d42da
MMaicke <mtmaicke@fullsail.edu>
2015-05-14 14:52:23 -0400

    addition

--- day5/lab5/lab5.js
+++ day5/lab5/lab5.js
@@ -7,21 +7,28 @@ var firstnumber  = parseFloat(prompt("enter the first number."));
 var secondnumber = parseFloat(prompt("enter the second number."));
 var symbol       = prompt("please enter + - / or *");
 
-
 //functions
 
-function add (add) {
+function addition (){
+var sumtotal = firstnumber + secondnumber;
 
+    return(firstnumber + " " + symbol + " " + secondnumber + " = " + sumtotal)
 }
-function subtract (minus){
-
+/*function subtract (minus){
+var total = firstnumber +- secondnumber;
+return minustotal
 }
 function multiply (times){
-
+    var total = firstnumber +* secondnumber;
+return multplytotal
 }
 function divide (divide){
-
+var total = firstnumber +/ secondnumber
+    return dividetotal
 }
+*/
+// main code
 
-
-console.log( firstnumber + ' ' + secondnumber )
+if( symbol === "+"){
+    console.log(addition())
+}
