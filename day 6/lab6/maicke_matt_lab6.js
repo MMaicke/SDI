2ca648d7c5725e4333f23bbc5e74d6f33a98bd75
MMaicke <mtmaicke@fullsail.edu>
2015-05-16 14:24:17 -0400

    lab done

--- day 6/lab6/maicke_matt_lab6.js
+++ day 6/lab6/maicke_matt_lab6.js
@@ -4,17 +4,20 @@ function lab6() {
 
     var teams = ["bears", "bulls", "cubs", "sox"];
 
-    var colors = ["orange", "red", "blue", "white"];
+    var colors = ["orange and blue", "red and white", "blue and white", "white and black"];
 
     for (var i = 0; i < teams.length; i++) {
 
 
-            console.log("the chicago " + teams[i] + " color is " + colors[i])
+            console.log("the chicago " + teams[i] + " colors are " + colors[i])
 
 
 
 
     }
+teams.push("fire");
+    colors.push("red and blue");
 
+    console.log("the chicago " + teams[i] + " colors are " + colors[i])
 }
 lab6();
