7e4214b36b60382a61d3fd45c8fcdf9d228a3ef8
MMaicke <mtmaicke@fullsail.edu>
2015-05-16 13:31:13 -0400

    lab

---
+++ day 6/js/script.js
@@ -0,0 +1,79 @@
+/**
+ * Created by matthewmaicke on 5/16/15.
+ */
+
+
+function myfirstarray(){
+    var PirateNames =["blackbeard","captian kidd","captain morgan","captain jack sparrow"];
+
+    console.log(PirateNames[3]);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////alert(" it works")
+//
+//
+//// var
+//var num1;
+//var num2
+//var operation
+//var final
+//
+//
+//
+////funct
+//function add(add1,add2){
+//
+//
+//}
+//
+//function subtract(sub1.sub2){
+//
+//
+//}
+//function multiply (mult1, multi2) {
+//}
+//
+//
+//function divide(div1,div2){
+//
+//}
+//
+////code
+//
+//num1 = parseFloat(promt("enter your first number"));
+//num2 = parseFloat(pormt("enter your second number"));
+//
+//operation = promt("what would you like to do +,-,/,*");
+//
+//if (operation = "+"){
+//add(num1,num2)
+//} else if  (operation = "-"){
+//
+//} else if ( operation= "/"){
+//
+//}
+//
+//else if (operation = "*"){
+//
+//}else console.log("im sorry you forgot to put a correct operation");
+
+
+
---
+++ day 6/lab6/maicke_matt_lab6.js
@@ -0,0 +1,13 @@
+//alert("JavaScript works!");
+
+function lab6(){
+
+    var teams = ["bears","bulls","cubs","sox"];
+
+    var colors = ["orange","red","blue","white"];
+
+    for (var i = 0; i < teams.length; i ++ ){
+
+
+    }
+}
