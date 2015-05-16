9036cbef9acc0dade1216066342955aa179251d2
MMaicke <mtmaicke@fullsail.edu>
2015-05-16 13:43:14 -0400

    lab part 2

--- day 6/day6.js
+++ day 6/day6.js
@@ -42,33 +42,33 @@ returnaverage= average();
 
 
 
-//alert('this works but why not the array');
-//
-//function myfirstarray(){
-//    var popped;
-//    var names = ["blackbeard","captian kidd","captain morgan","captain jack sparrow"];
-//
-//  for (var i = 0; i < names.length; i ++ ){
-//
-//      if (names[i] === "blackbeard"){
-//    console.log(names[i] + " is my favorite");
-//}
-//
-//
-//  }
-//
-//    console.log("the length of the arrays is " + names.length);
-//
-//
-//names.unshift("Harlock", "black bart");
-//
-//    console.log(names);
-//
-//    popped= names.pop();
-//
-//    console.log(names);
-//    console.log(popped+ "is the popped item")
-//
-//}
-//myfirstarray();
-//
+alert('this works but why not the array');
+
+function myfirstarray(){
+    var popped;
+    var names = ["blackbeard","captian kidd","captain morgan","captain jack sparrow"];
+
+  for (var i = 0; i < names.length; i ++ ){
+
+      if (names[i] === "blackbeard"){
+    console.log(names[i] + " is my favorite");
+}
+
+
+  }
+
+    console.log("the length of the arrays is " + names.length);
+
+
+names.unshift("Harlock", "black bart");
+
+    console.log(names);
+
+    popped= names.pop();
+
+    console.log(names);
+    console.log(popped+ "is the popped item")
+
+}
+myfirstarray();
+
--- day 6/lab6/maicke_matt_lab6.js
+++ day 6/lab6/maicke_matt_lab6.js
@@ -1,13 +1,19 @@
 //alert("JavaScript works!");
 
-function lab6(){
+function lab6() {
 
-    var teams = ["bears","bulls","cubs","sox"];
+    var teams = ["bears", "bulls", "cubs", "sox"];
 
-    var colors = ["orange","red","blue","white"];
+    var colors = ["orange", "red", "blue", "white"];
 
-    for (var i = 0; i < teams.length; i ++ ){
+    for (var i = 0; i < teams.length; i++) {
+
+        if (teams[i] === colors[i]) {
+            console.log("the chicago " + names[i] + " color is " + colors[i])
+
+        }
 
 
     }
+
 }
