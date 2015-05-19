797d8e34a556f3ac7001c11469ad6a134e3e6934
MMaicke <mtmaicke@fullsail.edu>
2015-05-19 12:30:12 -0400

    lab

--- day 6/lab6/maicke_matt_lab6.js
+++ day 6/lab6/maicke_matt_lab6.js
@@ -1,26 +1,26 @@
 //alert("JavaScript works!");
-//
-//function lab6() {
-//
-//    var teams = ["bears", "bulls", "cubs", "sox"];
-//
-//    var colors = ["orange and blue", "red and white", "blue and white", "white and black"];
-//
-//    for (var i = 0; i < teams.length; i++) {
-//
-//
-//            console.log("the chicago " + teams[i] + " colors are " + colors[i])
-//
-//
-//
-//
-//    }
-//teams.push("fire");
-//    colors.push("red and blue");
-//
-//    console.log("the chicago " + teams[i] + " colors are " + colors[i])
-//}
-//lab6();
+
+function lab6() {
+
+    var teams = ["bears", "bulls", "cubs", "sox"];
+
+    var colors = ["orange and blue", "red and white", "blue and white", "white and black"];
+
+    for (var i = 0; i < teams.length; i++) {
+
+
+            console.log("the chicago " + teams[i] + " colors are " + colors[i])
+
+
+
+
+    }
+teams.push("fire");
+    colors.push("red and blue");
+
+    console.log("the chicago " + teams[i] + " colors are " + colors[i])
+}
+lab6();
 
 //problem solve
 
--- day7/lab7/Maicke_Matthew_lab7.js
+++ day7/lab7/Maicke_Matthew_lab7.js
@@ -1,1 +1,21 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+
+//lab 7
+function lab7() {
+
+    var set1 = [10, 6, 7,3,12,5 ];
+
+    var set2 = [25, 100, 4,22, 16,105,2,7];
+
+    var set3 = [2,3,4,1,10,27,32,51,6,100,8,42];
+
+    for (var i = 0; i < set1.length; i++)
+
+        for (var i = 0; i < set2.length; i++)
+
+            for (var i = 0; i < set3.length; i++)
+
+
+
+    {
