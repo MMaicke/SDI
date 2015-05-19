5c4bd1decb527292008acd62015fab8a91f63998
MMaicke <mtmaicke@fullsail.edu>
2015-05-19 12:03:20 -0400

    lab

--- day7/day7js.js
+++ day7/day7js.js
@@ -34,18 +34,47 @@
 //gobal var
 var numberedarray = [ 1,2,3,4,5,6,7,8,9,10,11,12];
 var returnedarray = [];
+var userprompt = prompt("enter 'odd to return odd numbers or  'even' to return even numbers.")
+
+while(userprompt != "odd" && userprompt != "even"){ userprompt= prompt( "hey dumbass i said odd or even")
+
+
+}
+
 
 //function
 
-function evenarray ( argarray){
+function evenarray ( argarray,oddoreven){
     //local var
     var newarray= [];
     var modnum;
+    var zeroorone;
 
-console.log(argarray);
+    if (oddoreven === "odd") {
 
+        zeroorone = 1
+
+    }else {zeroorone = 0
+
+    }
+
+    for (var i =0; i < argarray.length; i++){
+
+        modnum = argarray[i] % 2;
+//even
+        if (modnum === zeroorone){
+
+            newarray.push(argarray[i]);
+
+        }
+
+    }
+//console.log(argarray);
+return newarray
 }
 
 //code
 
-returnedarray =evenarray(numberedarray)
+returnedarray = evenarray (numberedarray,userprompt);
+
+console.log( "the " +userprompt+ " array is " + returnedarray);
