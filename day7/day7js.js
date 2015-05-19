4dce318b323274f145a780a1125cf7616e50fe49
MMaicke <mtmaicke@fullsail.edu>
2015-05-19 10:22:27 -0400

    day 7

---
+++ day7/day7js.js
@@ -0,0 +1,51 @@
+/**
+ * Created by matthewmaicke on 5/19/15.
+ */
+
+//
+////alert("it works")
+//
+//
+//function authorfunction() {
+//
+//    var teams = ["bears", "bulls", "cubs", "sox"];
+//
+//    var colors = ["orange and blue", "red and white", "blue and white", "white and black"];
+//
+//    for (var i = 0; i < teams.length; i++) {
+//
+//        console.log("the chicago " + teams[i] + " colors are " + colors[i]+".");
+//
+//    }
+//
+//    teams.push("fire");
+//    colors.push("red and blue");
+//
+//
+//}
+//
+//
+//
+
+//problem
+
+
+
+//gobal var
+var numberedarray = [ 1,2,3,4,5,6,7,8,9,10,11,12];
+var returnedarray = [];
+
+//function
+
+function evenarray ( argarray){
+    //local var
+    var newarray= [];
+    var modnum;
+
+console.log(argarray);
+
+}
+
+//code
+
+returnedarray =evenarray(numberedarray)
