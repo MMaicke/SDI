f647a0c20ce442e17c22a60902aad6409ff28fa2
MMaicke <mtmaicke@fullsail.edu>
2015-05-21 11:00:28 -0400

    class

---
+++ day8/day8js.js
@@ -0,0 +1,59 @@
+///**
+// * Created by matthewmaicke on 5/21/15.
+// */
+////alert("it works")
+
+// going over last lab
+//
+////gobal var
+//
+//var set1 = [10, 6, 7, 3, 12, 5];
+//var set2 = [25, 100, 4, 22, 16, 105, 2, 7];
+//var set3 = [2, 3, 4, 1, 10, 27, 32, 51, 6, 100, 8, 42];
+//var returnarray;
+//var numarray= [set1,set2,set3];
+//
+//function bsort (argarray) {
+//
+////local var
+//    var num1, num2, holder;
+//
+////loop thru array
+//    for (var newelement in argarray) {
+//
+//        for (var element in argarray) {
+//
+//            element = parseInt(element);
+//
+//            // assisn arraly ele
+//            num1 = argarray[element];
+//            num2 = argarray[element + 1];
+//
+//
+//            if (num1 > num2) {
+//
+//
+//                holder = num1;
+//                argarray[element] = num2;
+//                argarray[element + 1] = num1;
+//
+//            }
+//            console.log(argarray)
+//
+//        }
+//    }
+//return argarray;
+//}
+//for (var each in  numarray) {
+//
+//    returnarray=bsort(numarray[each]);
+//
+//    console.log('array number' + each + 'sorted is' + returnarray);
+//
+//
+//
+//}
+//
+//
+//
+
