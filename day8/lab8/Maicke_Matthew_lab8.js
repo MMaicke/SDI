b49a5fedc420537304f864a10874ca2fb6e623da
MMaicke <mtmaicke@fullsail.edu>
2015-05-21 19:48:03 -0400

    more

--- day8/lab8/Maicke_Matthew_lab8.js
+++ day8/lab8/Maicke_Matthew_lab8.js
@@ -5,48 +5,89 @@
 
 
 //gobal var
-//
-//
-//var myemail = ("matthew_maickefullsailedu");
-//
-//
-//    //function
-//function indexat (whereIsIt){
-//
-//
-//    var where = whereIsIt.lastIndexOf("@");
-//    var where2 = whereIsIt.lastIndexOf(".")
-//
-//
-//    return where, where2;
-//
-//if (indexat = (-1));
-//
-//    return console.log('invalid email')
-//
-//
-//
-//}
-//
-//    console.log("the index is "+ indexat(myemail));
-//
-//
+
+var myemail= "matthew_maicke@fullsail.edu";
+
+var returnemail;
+
+
+//function
+
+function emails(argemail){
+    //loc var
+
+    var at = argemail.indexOf("@");
+    var dot = argemail.indexOf(".");
+    var secondat = argemail.lastIndexOf("@");
+    var space = argemail.indexOf(" ");
+
+
+    if (at == -1) {
+
+        return false
+
+    }
+
+    else if (dot == -1){
+
+        return false
+    }
+
+    else { return true}
+}
+returnedstring=emails(myemail);
+console.log("my email is" + returnemail)
 
 //prob 2
 
 
 //problem 2
 
-
-var bombermotto; 
-
+var bombermotto;
 var comma;
+var slash;
 
- var slash; 
+bombermotto = "Bombs,on,target";
+comma = ",";
+slash = " ";
 
- bombermotto = "bombs,on,target";
+function hatethislab(first, second, third){
+    var newmotto;
+    var spacemotto;
+    var current;
+    var seprate = "";
 
- comma = ",";
+    newmotto = first;
 
- slash = " ";
+    spacemotto = newmotto.split(second);
 
+    for(var all in spacemotto){
+
+        current = spacemotto[all];
+
+        for(var char in spacemotto[all]){
+
+            if (char != 0){
+
+                var currentchar = current[char];
+                seprate = seprate + currentchar;
+
+
+            } else {
+                seprate = seprate + current[0];
+
+            }
+
+        }
+        seprate = seprate + third;
+
+    }
+    return seprate;
+
+}
+
+hatethislab(bombermotto, comma, slash);
+
+var returnval = hatethislab(bombermotto, comma, slash);
+
+console.log("Normally bomber squadron mottos is " + returnval + " but Minot's motto is bombs on foreheads");
