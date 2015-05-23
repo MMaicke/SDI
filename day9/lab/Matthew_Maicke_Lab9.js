2a14f495bb79a6e42406e005ec073fcbbc5e578b
MMaicke <mtmaicke@fullsail.edu>
2015-05-23 13:54:44 -0400

    lab 2 questions done

--- day9/lab/Matthew_Maicke_Lab9.js
+++ day9/lab/Matthew_Maicke_Lab9.js
@@ -1,39 +1,63 @@
 //alert("JavaScript works!");
 
+//prob 1
+//var number = 1;
+//var decminal= 3
+//function dec(argdec){
+//
+//
+//    newnumber = number.toFixed(decminal);
+//
+//
+//
+//return newnumber
+//
+//
+//
+//}
+//
+//dec();
+//
+//console.log("In video games anything prior to "+ newnumber+ " versions of a game is usually demo or some kind of trial like betas or alphas");
+
+
+
+//prob 2
 
 
 
 
-
-//prob 3
-
-
-
-
-
-function days(argday){
-
-
-
-    var firstdate = new Date(2015, 5, 23);
-
-    var seconddate = new Date (2015, 10, 23);
-
-
-    dateDiff = firstdate-seconddate;
-
-    dateDiff= dateDiff / 1000 / 60 / 60 / 24;
-
-    day = Math.floor(Math.abs(dateDiff));
-
-
-    return day
-
-
-
-}
-
-
-days();
-
-console.log("As of May 23 there are " + day + " days till the release of Call of Duty Black Ops III");
+//
+//
+////prob 3
+//
+//
+//
+//
+////func
+//function days(argday){
+//
+//
+////local var
+//    var firstdate = new Date(2015, 5, 23);
+//
+//    var seconddate = new Date (2015, 10, 23);
+//
+//
+//    dateDiff = firstdate-seconddate;
+//
+//    dateDiff= dateDiff / 1000 / 60 / 60 / 24;
+//
+//    day = Math.floor(Math.abs(dateDiff));
+//
+//
+//    return day
+//
+//
+//
+//}
+//
+//
+//days();
+//
+//console.log("As of May 23 there are " + day + " days till the release of Call of Duty Black Ops III");
