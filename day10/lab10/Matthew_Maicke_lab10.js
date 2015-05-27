3d4e5d2a3cd35bba3458f19aff6bbc8e1d37cb39
MMaicke <mtmaicke@fullsail.edu>
2015-05-26 21:16:31 -0400

    next part

--- day10/lab10/Matthew_Maicke_lab10.js
+++ day10/lab10/Matthew_Maicke_lab10.js
@@ -13,24 +13,30 @@ function lotto (arggen) {
     if (arggen === quick){
 
 
-        for (var j = 0; j <= 6; j++) {
+        for (var i = 0; i < 7; i++) {
 
-            // Gets a random number for each pass through the loop
-            array[j] = Math.floor((Math.random() * 53) + 1);
+
+            array[i] = Math.floor((Math.random() * 53) + 1);
         }
 
     }
+    else if (arggen === power) {
 
-    
+        for (var i = 0; i < 6; i++) {
 
+            array[i] = Math.floor((Math.random()) * 59 + 1);
 
-
-
-
-
-
-
+        }
+        array[i] = Math.floor((Math.random()) * 35 + 1);
 
 
+
+    }
+return array
 
 }
+
+
+input = prompt("for quick pick numbers enter 'q' or for  power ball enter 'p'");
+
+
