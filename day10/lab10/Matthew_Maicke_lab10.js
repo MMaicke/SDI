1ab3e0545f4c236e2fa27e778e970c4a788bf8e6
MMaicke <mtmaicke@fullsail.edu>
2015-05-26 20:38:29 -0400

    1

--- day10/lab10/Matthew_Maicke_lab10.js
+++ day10/lab10/Matthew_Maicke_lab10.js
@@ -1,3 +1,31 @@
 //alert("JavaScript works!");
 
 
+var returnedLot;
+var input;
+var power = "p";
+var quick = "q";
+
+function lotto (arggen) {
+
+    var array = [];
+
+    if (argLotto === power) {
+
+
+
+
+
+
+        
+    }
+
+
+
+
+
+
+
+
+
+}
